3c391ecd025ba69bae88d2609fdd1af26bb45b97
Nalanihv <nalanihevi@gmail.com>
2022-03-28 11:56:28 -0600

    hakerrang actividad

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static List<Integer>compareTripes(List<Integer>a, List<Integer>b){
+        //a=1,2,3
+        //b=3,2,1
+        int contA=0, contB=0;
+        for (int i = 0; i < a.size(); i++) {
+            if(a.get(i)>b){
+                contA++
+            }else if{(b.get(i)>a.get(i)){
+                contB++;
+            }else{}
+            }
+        }
+    }
+
+    ArrayList<Integer>lista=new ArrayList<>(Arrays.asList(contA,contB));
+
+}
